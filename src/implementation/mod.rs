e93f771c4b505fe05a8034342ee5f6d880dce7ad
Raphael <raphael.goetz.7@gmail.com>
2025-06-03 17:32:27 +0200

    fix: removed boolean

--- src/implementation/mod.rs
+++ src/implementation/mod.rs
@@ -1,13 +1,11 @@
 use crate::registry::HandlerFn;
 
-pub mod boolean;
 pub mod number;
 
 pub fn collect() -> Vec<(&'static str, HandlerFn)> {
     let mut result = vec![];
 
     result.extend(number::collect_number_functions());
-    result.extend(boolean::collect_boolean_functions());
 
     result
 }

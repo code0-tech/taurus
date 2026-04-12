2c25139839c302933a8e73c7ad2ada30d1d34da1
Raphael <raphael.goetz.7@gmail.com>
2026-04-12 14:34:43 +0200

    feat: added auth header to status and usage

--- crates/taurus/src/client/runtime_usage.rs
+++ crates/taurus/src/client/runtime_usage.rs
@@ -1,5 +1,7 @@
-use code0_flow::flow_service::retry::create_channel_with_retry;
-use tonic::transport::Channel;
+use code0_flow::flow_service::{
+    auth::get_authorization_metadata, retry::create_channel_with_retry,
+};
+use tonic::{Extensions, Request, transport::Channel};
 use tucana::{
     aquila::{RuntimeUsageRequest, runtime_usage_service_client::RuntimeUsageServiceClient},
     shared::RuntimeUsage,
@@ -7,21 +9,29 @@ use tucana::{
 
 pub struct TaurusRuntimeUsageService {
     channel: Channel,
+    aquila_token: String,
 }
 
 impl TaurusRuntimeUsageService {
-    pub async fn from_url(aquila_url: String) -> Self {
+    pub async fn from_url(aquila_url: String, aquila_token: String) -> Self {
         let channel = create_channel_with_retry("Aquila", aquila_url).await;
-        TaurusRuntimeUsageService { channel }
+        TaurusRuntimeUsageService {
+            channel,
+            aquila_token,
+        }
     }
 
     pub async fn update_runtime_usage(&self, runtime_usage: RuntimeUsage) {
         log::info!("Updating the current Runtime Status!");
         let mut client = RuntimeUsageServiceClient::new(self.channel.clone());
 
-        let request = RuntimeUsageRequest {
-            runtime_usage: vec![runtime_usage],
-        };
+        let request = Request::from_parts(
+            get_authorization_metadata(&self.aquila_token),
+            Extensions::new(),
+            RuntimeUsageRequest {
+                runtime_usage: vec![runtime_usage],
+            },
+        );
 
         match client.update(request).await {
             Ok(response) => {
